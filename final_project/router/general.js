c5017748978157141619384368943e3a3b4e5c21
Felix Zie <felixwwzie@gmail.com>
2026-04-23 22:24:59 +0800

    add promises or async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const axios = require("axios");
+const Base_URL = "http://localhost:5001";
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -37,47 +39,84 @@ public_users.post("/register", (req, res) => {
   return res.status(404).json({ message: "Unable to register user." });
 });
 
-// Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  res.send(JSON.stringify(books, null, 4));
+// Get the book list available in the shop using Async/Await
+public_users.get("/", async (req, res) => {
+  try {
+    const getBooks = () => Promise.resolve(books);
+    const bookList = await getBooks;
+    res.status(200).send(JSON.stringify(books, null, 4));
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error fetching books",
+      error: err.message,
+    });
+  }
 });
 
-// Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+// Get book details based on ISBN using Promises
+public_users.get("/isbn/:isbn", async (req, res) => {
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  const findByIsbn = new Promise((resolve, reject) => {
+    const book = books[isbn];
+    if (book) {
+      resolve(book);
+    } else {
+      reject("Book not found");
+    }
+  });
+
+  findByIsbn
+    .then((book) => res.status(200).json(book))
+    .catch((err) => res.status(404).json({ message: "Error fetching book by isbn", error: err.message }));
 });
 
-// Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+// Get book details based on author using Async/Await
+public_users.get("/author/:author", async (req, res) => {
   const author = req.params.author;
-  const keys = Object.keys(books);
-  const filteredBooks = [];
-  keys.forEach((key) => {
-    if (books[key].author === author) {
-      filteredBooks.push(books[key]);
-    }
-  });
-  res.send(JSON.stringify(filteredBooks, null, 4));
+  try {
+    const getByAuthor = () => {
+      return new Promise((resolve) => {
+        const keys = Object.keys(books);
+        const filteredBooks = keys.filter((key) => books[key].author === author).map((key) => books[key]);
+        resolve(filteredBooks);
+      });
+    };
+
+    const result = await getByAuthor();
+    res.status(200).json(result);
+  } catch (errerror) {
+    res.status(500).json({ message: "Error fetching book by author", error: err.message });
+  }
 });
 
-// Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+// Get all books based on title using Async/Await
+public_users.get("/title/:title", async (req, res) => {
   const title = req.params.title;
-  const keys = Object.keys(books);
-  const filteredBooks = [];
-  keys.forEach((key) => {
-    if (books[key].title === title) {
-      filteredBooks.push(books[key]);
-    }
-  });
-  res.send(JSON.stringify(filteredBooks, null, 4));
+  try {
+    const getByTitle = () => {
+      return new Promise((resolve) => {
+        const keys = Object.keys(books);
+        const filteredBooks = keys.filter((key) => books[key].title === title).map((key) => books[key]);
+        resolve(filteredBooks);
+      });
+    };
+
+    const result = await getByTitle;
+    res.status(200).json(result);
+  } catch (errerror) {
+    res.status(500).json({ message: "Error fetching book by author", error: err.message });
+  }
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn].reviews);
+  const book = books[isbn];
+  if (book) {
+    res.status(200).json(book.reviews);
+  } else {
+    res.status(404).json({ message: "No review found for this isbn", error: err.message });
+  }
 });
 
 module.exports.general = public_users;
